26cf49491ca89b1003074cad863dfd7a9e12be64
Cameron Stewart <cg@rocktownlabs.com>
2026-01-06 18:20:11 -0600

    Fix env configuration for @t3-oss/env-core

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -2,10 +2,13 @@ import { createEnv } from "@t3-oss/env-core"
 import { z } from "zod"
 
 export const env = createEnv({
+  clientPrefix: "NEXT_PUBLIC_",
   client: {
     NEXT_PUBLIC_API_URL: z.string().url().optional(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
 })
