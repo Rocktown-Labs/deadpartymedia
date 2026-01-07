62fc3b066bf0fd6f0d84bb4f44cff76670dfbec3
Cameron Stewart <cg@rocktownlabs.com>
2026-01-06 18:08:46 -0600

    Fix TypeScript: install missing Radix UI packages, fix button-group types

--- apps/web/src/components/ui/button-group.tsx
+++ apps/web/src/components/ui/button-group.tsx
@@ -25,7 +25,7 @@ function ButtonGroup({
   className,
   orientation,
   ...props
-}: React.ComponentProps<'div'> & VariantProps<typeof buttonGroupVariants>) {
+}: React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof buttonGroupVariants>) {
   return (
     <div
       role="group"
@@ -41,7 +41,7 @@ function ButtonGroupText({
   className,
   asChild = false,
   ...props
-}: React.ComponentProps<'div'> & {
+}: React.HTMLAttributes<HTMLDivElement> & {
   asChild?: boolean
 }) {
   const Comp = asChild ? Slot : 'div'
@@ -61,7 +61,7 @@ function ButtonGroupSeparator({
   className,
   orientation = 'vertical',
   ...props
-}: React.ComponentProps<typeof Separator>) {
+}: React.ComponentPropsWithoutRef<typeof Separator>) {
   return (
     <Separator
       data-slot="button-group-separator"
